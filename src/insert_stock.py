22dcbfd0706df7b9b6d6126bbd54ea293c56ab18
Kyle Aundry <kyleaundry15@gmail.com>
2026-06-22 02:23:44 +0900

    Minor code changes

--- src/insert_stock.py
+++ src/insert_stock.py
@@ -1,10 +1,10 @@
 from db_connection import get_db_connection
 
-
 conn = get_db_connection()
 print("Connected successfully!")
 
 cur = conn.cursor()
+
 cur.execute(
     "INSERT INTO stock_prices (ticker, company_name, trade_date, open_price, close_price, high_price, low_price, volume) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
     ("MYNLD", "Maynilad Water Services, Inc.", "2026-06-19", 22.45, 22.40, 22.70, 21.70, 110390900)
