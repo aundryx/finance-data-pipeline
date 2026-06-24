73a13adc3a2dd76b24b56f4122b8e5c315a99622
Kyle Aundry <kyleaundry15@gmail.com>
2026-06-25 06:18:09 +0900

    Remove old migrations.sql replaced by migrations subfolder

--- sql/migrations.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Added unique constraint to stock_prices table to prevent duplicate entries based on ticker and trade_date
--- Date: 2024-06-25
-
-ALTER TABLE stock_prices
-ADD CONSTRAINT unique_ticker_date UNIQUE (ticker, trade_date);
