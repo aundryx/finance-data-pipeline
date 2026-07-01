d96656bde6f4742a0183304b4f0e2d6e08c6e2ef
Kyle Aundry <kyleaundry15@gmail.com>
2026-07-01 19:31:15 +0800

    Added the function for AWS RDS connection in db_connection.py

--- src/db_connection.py
+++ src/db_connection.py
@@ -4,6 +4,7 @@ import os
 
 load_dotenv()
 
+# Local postgres database connection function
 def get_db_connection():
     conn = psycopg2.connect(
         dbname = os.getenv("DB_NAME"),
@@ -11,6 +12,18 @@ def get_db_connection():
         password = os.getenv("DB_PASSWORD"),
         host = os.getenv("DB_HOST"),
         port = os.getenv("DB_PORT")
+    )
+
+    return conn
+
+# AWS RDS postgres database connection function
+def get_rds_connection():
+    conn = psycopg2.connect (
+        dbname = os.getenv("RDS_DB_NAME"),
+        user = os.getenv("RDS_DB_USER"),
+        password = os.getenv("RDS_DB_PASSWORD"),
+        host = os.getenv("RDS_DB_HOST"),
+        port = os.getenv("RDS_DB_PORT")
     )
 
     return conn
