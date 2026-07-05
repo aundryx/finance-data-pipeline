55b696d3d2b3ca7846f5f96dbe256d27c368f15e
Kyle Aundry <kyleaundry15@gmail.com>
2026-07-06 00:32:30 +0800

    Updated the pipeline to also load stock data into snowflake

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,7 +2,7 @@ import logging
 import os
 import json
 from fetch_stock_data import fetch_stock_data
-from db_connection import get_rds_connection
+from db_connection import get_rds_connection, get_snowflake_connection
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
@@ -37,9 +37,12 @@ def main():
     logger = setup_logging() 
 
     conn = get_rds_connection()
-    logger.info("Connected to the database")
+    logger.info("Connected to AWS Postgres database")
+    conn_snowflake = get_snowflake_connection()
+    logger.info("Connected to Snowflake")
 
     cursor = conn.cursor()
+    cursor_snowflake = conn_snowflake.cursor()
 
     config = load_config()
 
@@ -58,15 +61,24 @@ def main():
                     (row['ticker'], row['company_name'], row['trade_date'], row['open_price'], row['close_price'], row['high_price'], row['low_price'], row['volume']) 
                 )
 
+                cursor_snowflake.execute(
+                    "MERGE INTO stock_prices AS target USING (SELECT %s AS ticker, %s AS company_name, %s AS trade_date, %s AS open_price, %s AS close_price, %s AS high_price, %s AS low_price, %s AS volume) AS source ON target.ticker = source.ticker AND target.trade_date = source.trade_date WHEN NOT MATCHED THEN INSERT (ticker, company_name, trade_date, open_price, close_price, high_price, low_price, volume) VALUES (source.ticker, source.company_name, source.trade_date, source.open_price, source.close_price, source.high_price, source.low_price, source.volume)",
+                    (row['ticker'], row['company_name'], row['trade_date'], row['open_price'], row['close_price'], row['high_price'], row['low_price'], row['volume'])
+                )
+
             conn.commit()
+            conn_snowflake.commit()
             logger.info(f"Successfully loaded {len(stock_to_fetch)} rows for {stock['ticker_symbol']}")
 
         except Exception as e:
             logger.error(e)
             continue
 
+
     cursor.close() 
     conn.close()
+    cursor_snowflake.close()
+    conn_snowflake.close()
     logger.info("Pipeline completed")
         
 
