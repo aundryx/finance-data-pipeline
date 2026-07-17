ada98e93df0d93c6625e0907476f092c6490fcd9
Kyle Aundry <kyleaundry15@gmail.com>
2026-07-18 04:11:20 +0800

    Fixing again the path of config.json

--- dags/stock_pipeline_dag.py
+++ dags/stock_pipeline_dag.py
@@ -5,7 +5,7 @@ import os
 import json
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
-config_path = os.path.join(base_dir, '..', 'config.json')
+config_path = os.path.join(base_dir, "..", "config.json")
 with open(config_path, "r") as f:
     config = json.load(f)
 
