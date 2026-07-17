febd332df53439d8ff73b566c7680bc2398300bb
Kyle Aundry <kyleaundry15@gmail.com>
2026-07-18 00:58:41 +0800

    Created the script that handle the flow and execution of DAG tasks

---
+++ dags/stock_pipeline_dag.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner' : 'airflow',
+    'retries' : 2,
+    'retry_delay' : timedelta(minutes=2),
+    'email_on_failure' : True,
+    'email' : ['kyleaundryx@gmail.com', 'kyleaundry15@gmail.com']
+}
+  
+with DAG (
+    dag_id = "stock_pipeline",
+    schedule = '0 6 * * MON-FRI',
+    start_date = datetime(2026, 7, 1),
+    catchup = False,
+    default_args = default_args
+) as dag:
+    task1 = BashOperator(
+        task_id = 'fetch_and_transform',
+        bash_command = 'source /home/ubuntu/finance-data-pipeline/venv/bin/activate && python3 /home/ubuntu/finance-data-pipeline/src/fetch_transform.py'
+    )
+
+    task2 = BashOperator(
+        task_id = 'load_to_rds',
+        bash_command = 'source /home/ubuntu/finance-data-pipeline/venv/bin/activate && python3 /home/ubuntu/finance-data-pipeline/src/load_rds.py'
+    )
+
+    task3 = BashOperator(
+        task_id = 'load_to_snowflake',
+        bash_command = 'source /home/ubuntu/finance-data-pipeline/venv/bin/activate && python3 /home/ubuntu/finance-data-pipeline/src/load_snowflake.py'
+    )
+
+    task1 >> [task2, task3]
