37dc48a04fd158399764949ed7ad8a065b20d60b
Kyle Aundry <kyleaundry15@gmail.com>
2026-07-22 04:08:48 +0800

    Remove the logging setup in pipeline.py and import the setup_logging() from logger_setup.py instead for reusability

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,37 +1,8 @@
-import logging 
-import os
-import json
 from fetch_stock_data import fetch_stock_data
 from db_connection import get_rds_connection, get_snowflake_connection
+from logger_setup import setup_logging
+from json_loader import load_config
 
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
-CONFIG_PATH = os.path.join(BASE_DIR, "..", "config.json") 
-logs_dir = os.path.join(BASE_DIR, "..", "logs")
-
-def setup_logging():
-    os.makedirs(logs_dir, exist_ok=True) 
-
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-
-    file_handler = logging.FileHandler(os.path.join(logs_dir, "pipeline.log")) 
-    file_handler.setFormatter(formatter) 
-
-    console_handler = logging.StreamHandler() 
-    console_handler.setFormatter(formatter)
-
-   
-    logger = logging.getLogger("stock_pipeline")
-    logger.setLevel(logging.INFO)
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
-
-    return logger
-
-def load_config():
-    with open(CONFIG_PATH, "r") as f:
-        config = json.load(f)
-    return config
 
 def main():
     logger = setup_logging() 
